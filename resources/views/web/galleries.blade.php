09c9f954139172f21c7a130955c67daa2b67d22d
sabreenMohammed107 <sabreenm312@gmail.com>
2022-04-16 02:09:07 +0200

    test

--- resources/views/web/galleries.blade.php
+++ resources/views/web/galleries.blade.php
@@ -60,9 +60,9 @@
                                                 </div>
                                             </a>
                                         @else
-                                            {{-- <a class="gallery image-popup img d-flex align-items-center"
-                                                href="{{ $row->path }}"> --}}
-                                                <iframe src="{{$row->path}}" style=" width: 100%;
+                                            <a class="gallery image-popup img d-flex align-items-center"
+                                                href="{{ $row->path }}">
+                                                <iframe src="{{ $row->path }}" style=" width: 100%;
                                         height: 100%;
                                         position: absolute;
                                         object-fit: cover;
@@ -70,8 +70,8 @@
                                                 <div>
                                                     <i class="fa fa-play text-white fz-18"></i>
                                                 </div>
-                                                {{-- </div> --}}
-                                                {{-- {{ $row->path }}</a> --}}
+
+                                                {{ $row->path }}</a>
                                         @endif
                                     </div>
                                 </div>
